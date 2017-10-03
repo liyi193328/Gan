f8298f28502866358fea8c5419a1f1999ec95ad9
klovbe <58441556@qq.com>
2017-10-03 19:45:01 +0800

    Merge branch 'master' of https://github.com/liyi193328/Gan
    # Conflicts:
    #	autoencoder.py

--- plot.py
+++ plot.py
@@ -19,6 +19,7 @@ def plot_save(test_path_or_df, infer_path_or_df, save_path):
     sample_df = test_path_or_df
     infer_df = infer_path_or_df
 
+  magic_df = pd.read_csv("/home/bigdata/cwl/Gan/prediction/drop80_magic.csv", header=None)
   base_index = 2
 
   # print("len sample_df: {}".format(len(sample_df)))
@@ -26,8 +27,10 @@ def plot_save(test_path_or_df, infer_path_or_df, save_path):
   indexs = np.arange(base_index, len(sample_df), 10)
   sample_df = sample_df.iloc[indexs]
   infer_df = infer_df.iloc[indexs]
+  magic_df = magic_df.iloc[indexs]
 
   or_df = pd.read_csv("/home/bigdata/cwl/data_preprocessed/expx_dropprocessed.csv", header=0, sep=",", index_col=0)
+  magic_df.columns = or_df.columns
   # print(sample_df.shape, infer_df.shape, or_df.shape)
   # Two subplots, the axes array is 1-d
   feature_indexs = [3, 100, 500, 1000, 4000, 6000, 8000, 10000]
@@ -35,14 +38,16 @@ def plot_save(test_path_or_df, infer_path_or_df, save_path):
   x = np.arange(0, len(indexs), 1)
   for feature_index in feature_indexs:
     column = columns[feature_index]
-    f, axarr = plt.subplots(3, sharex=True)
+    f, axarr = plt.subplots(2,2,sharex=True)
     plt.xlim(-1, len(infer_df) + 1)
-    axarr[0].set_title("origin: {}".format(column))
-    axarr[0].scatter(x, or_df[or_df.columns[feature_index]])
-    axarr[1].scatter(x, sample_df[column])
-    axarr[1].set_title('drop: {}'.format(column))
-    axarr[2].set_title("infer: {}".format(column))
-    axarr[2].scatter(x, infer_df[column])
+    axarr[0,0].set_title("original: {}".format(column))
+    axarr[0,0].scatter(x, or_df[or_df.columns[feature_index]])
+    axarr[0,1].set_title('dropout: {}'.format(column))
+    axarr[0,1].scatter(x, sample_df[column])
+    axarr[1,0].set_title('magic: {}'.format(column))
+    axarr[1,0].scatter(x, magic_df[column])
+    axarr[1,1].set_title("autoencoder: {}".format(column))
+    axarr[1,1].scatter(x, infer_df[column])
 
   # print(plt.get_fignums())
 
